bfc4197ec9b529b2dc2414a7159f0c733c00321b
decagon <samuel.okafor@decagon.dev>
2023-08-29 03:45:48 +0100

    update

---
+++ src/main/java/com/example/jwtspringsecurity/Service/AuthenticationService.java
@@ -0,0 +1,35 @@
+package com.example.jwtspringsecurity.Service;
+
+import com.example.jwtspringsecurity.Config.JwtService;
+import com.example.jwtspringsecurity.Dto.AuthenticationRequest;
+import com.example.jwtspringsecurity.Dto.AuthenticationResponse;
+import com.example.jwtspringsecurity.Dto.RegisterRequest;
+import com.example.jwtspringsecurity.Entity.User;
+import com.example.jwtspringsecurity.Enum.Role;
+import com.example.jwtspringsecurity.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .firstName(registerRequest.getFirstName())
+                .lastName(registerRequest.getLastName())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+    }
+}
