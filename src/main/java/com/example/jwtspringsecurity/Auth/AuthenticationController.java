a4c77f8000a9e2dcdc9058ba0c41f9895a2feb32
decagon <samuel.okafor@decagon.dev>
2023-08-29 03:46:37 +0100

    update

---
+++ src/main/java/com/example/jwtspringsecurity/Auth/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.example.jwtspringsecurity.Auth;
+
+import com.example.jwtspringsecurity.Dto.AuthenticationRequest;
+import com.example.jwtspringsecurity.Dto.AuthenticationResponse;
+import com.example.jwtspringsecurity.Dto.RegisterRequest;
+import com.example.jwtspringsecurity.Service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest){
+     return ResponseEntity.ok(authenticationService.register(registerRequest));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest){
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+
+    }
+}
