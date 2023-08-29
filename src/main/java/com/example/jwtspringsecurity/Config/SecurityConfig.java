bcfb5903cf89b90f87fd03e50dcdae2bf9832cd8
decagon <samuel.okafor@decagon.dev>
2023-08-29 04:03:05 +0100

    update

--- src/main/java/com/example/jwtspringsecurity/Config/SecurityConfig.java
+++ src/main/java/com/example/jwtspringsecurity/Config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
