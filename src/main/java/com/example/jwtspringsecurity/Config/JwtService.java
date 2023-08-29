ed20023a18525eebe1fd705022cf11bff223be17
decagon <samuel.okafor@decagon.dev>
2023-08-29 02:16:32 +0100

    update

--- src/main/java/com/example/jwtspringsecurity/Config/JwtService.java
+++ src/main/java/com/example/jwtspringsecurity/Config/JwtService.java
@@ -29,17 +29,18 @@ public class JwtService {
     public String generateToken(UserDetails userDetails){
         return generateToken(new HashMap<>(), userDetails);
     }
-//    public boolean isTokenValid(String token, UserDetails userDetails){
-//        final String username = extractUsername(token);
-//        return(username.equals(userDetails.getUsername())) && !isTokenExpired(token);
-//    }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return(username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
 
-//    private boolean isTokenExpired(String token) {
-//        return extractExpiration(token).before(new Date());
-//    }
-//
-//    private  extractExpiration(String token) {
-//    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
     public String generateToken(Map<String, Object> extractClaims,
                                 UserDetails userDetails){
